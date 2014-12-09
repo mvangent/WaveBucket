81a63640d13e703afea4df5c29f733d5a197eaab
Melvin van Gent <mvgent@sogyo.nl>
2014-12-09 15:01:31 +0100

    subscription at index now links to identity

---
+++ Senken/Senken.Tests/Controllers/AccountControllerTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System.Globalization;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using Senken.Controllers;
+using Senken.Models;
+
+
+namespace Senken.Tests.Controllers
+{
+    /// <summary>
+    /// Summary description for UnitTest1
+    /// </summary>
+    [TestClass]
+    public class AccountControllerTest
+    {
+        
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
